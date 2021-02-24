eb15d3cc9fd23ae59c752a2b018e557fcceb487d
customsocks <customsocks@protonmail.com>
2021-02-24 21:47:53 +0000

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import 'bootstrap/dist/css/bootstrap.css';
 import App from './components/App';
-import configureStore from './store/configureStore'
-import reportWebVitals from './reportWebVitals';
+import configureStore from './store/configureStore';
+// import reportWebVitals from './reportWebVitals';
 import * as serviceWorker from './serviceWorker'
 
 //const store = configureStore()
@@ -19,5 +19,5 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
 serviceWorker.unregister()
